a0c1fcc9533ea06046f6f5674180651e044ef64d
asingh20-tech <asingh20@oswego.edu>
2026-01-10 00:55:33 -0500

    made names.py folder and made some open method use

--- names.py
+++ names.py
@@ -1,15 +1,42 @@
-import requests
-import sys
-import json
-import cowsay
+names = []
+students =[]
 
-if len(sys.argv)!= 2:
-    sys.exit()
+def main():
+    csv_arrangemnet()
 
- 
- 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
-o  = response.json()
+def append_files():
+        for _ in range(3):
+            with open("names.txt","a") as file:
+                name = input("whats your name ?")
+                file.write(f"{name}\n")
 
-for result in o["results"]:
-    print(result["trackName"])
+def read_file():
+        with open("names.txt","r") as file:
+             
+            #method 2 
+            for line in sorted(file):
+                 print("hello", line.rstrip())  
+             
+        #Method 1     
+        #     lines= file.readlines()
+        # for line in lines:
+        #     print(f"hello,{line}",end="",sep=",") 
+
+def csv_arrangemnet():
+     with open ("student.csv") as file: 
+          for line in file:
+               first,last = line.rstrip().split(",")  
+               student ={}
+               student["name"] = first
+               student["house"] = last
+               students.append(student) 
+
+for student in students:
+     print(f"{student['first']},{student['last']}")                  
+
+if __name__ == "__main__":
+    main()    
+
+
+     
+          
