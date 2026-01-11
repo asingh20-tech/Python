a82d9e0f1923de18a811c80afed4aac0563ca2cd
asingh20-tech <asingh20@oswego.edu>
2026-01-11 18:55:50 -0500

    went forward with OOP and learned that we can make our own exception learnes __str__ which is tostring in java

--- students.py
+++ students.py
@@ -1,21 +1,45 @@
 # docs.python.org/3/tutorial/classes.html
+
+# to string in java __str__ in py
+
 class Student:
-    def __init__(self, name , house):
+    def __init__(self, name , house, patronus):
+        # so with oop it helps us to make our own exceptions
+        if not name:
+            raise ValueError("Missing Name")
+        if house not in ["Gryffindor","Slytherin","RavenClaw","HufflePuff"]:
+            raise ValueError("Invalid House")
+        
         self.name = name 
         self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} is {self.house}"
+    
+    def charm(self):
+        match self.patronus: #its like switch in java
+            case "Stag":
+                return "🐴"
+            case "Otter":
+                return "🦦"
+            case _:
+                return "🪄 "
 
 def main():
     student = get_student()
     if student.name == "Padma":
         student.house = "Ravenclaw"
-    print(f"{student.name} from {student.house}")
+    print(f"Expecto patronum!")
+    print (student.charm())    
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name,house)
+    patronus = input ("Patronus : ")
+    student = Student(name,house,patronus)
     return student
-
 
 if __name__ == "__main__":
     main() 
