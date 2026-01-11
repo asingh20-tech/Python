8a64ac1bbd7a5accd2dfccf622f00c93877d7274
asingh20-tech <asingh20@oswego.edu>
2026-01-11 12:19:37 -0500

    change in the regex for email

--- validate.py
+++ validate.py
@@ -3,10 +3,12 @@ import re
 
 email = input("whats your email?").strip()
 
-if re.search(r"^[a-zA-Z0-9_].+{@}.+\.edu$",email):
+if re.search(r"^\w.+@(\w+\.)?\w+\.edu$",email, re.IGNORECASE):#re.IGNORECASE,re.MULTILINE,re.DOTALL
     print("Valid")
 else :
     print("Invalid")   
 
 
     #  [] set of characters , [^] complementing the set
+    # \d decimal digit , \s whitespace character, \w word charcater numbers and underscore or [a-zA-Z0-9_ ]
+    # A|B either a or b ,, (...) a group  (?...) non-capturing version
