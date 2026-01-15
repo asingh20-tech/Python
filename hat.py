991a9baaba6cc65506d8bd337d22f2c432bb7cb0
asingh20-tech <asingh20@oswego.edu>
2026-01-14 23:34:02 -0500

    Made a file called hat.py and made a class

---
+++ hat.py
@@ -0,0 +1,9 @@
+class Hat:
+    def __init__(self):
+        self.houses = ["Gryffindor" ]
+    def sort(self, name):
+        print(name , "is in", "some house")
+
+
+hat = Hat()
+hat.sort("Harry")    
