8a8007cf13c9f1cb344b9a3cefc5add031469d33
asingh20-tech <asingh20@oswego.edu>
2026-01-18 11:38:24 -0500

    made the classmethod and remove the __init__ method for intialising and also using cls

--- hat.py
+++ hat.py
@@ -1,11 +1,11 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Gryffindor","Hufflepuff","Ravenclaw" ]
-    def sort(self, name):
-        print(name , "is in", random.choice(self.houses))
+    
+    houses = ["Gryffindor","Hufflepuff","Ravenclaw" ]
+    @classmethod
+    def sort(cls, name):
+        print(name , "is in", random.choice(cls.houses))
 
 
-hat = Hat()
-hat.sort("Harry")    
+Hat.sort("Harry")    
